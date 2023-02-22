7594ce791555b440be3e76ce2fa0b082c7fba97e
Logan Graham <LDGBK6@mail.missouri.edu>
2023-02-22 16:31:21 -0600

    Fixed file paths

--- waldo_finder/waldo_finder.py
+++ waldo_finder/waldo_finder.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy
 
-img_rgb = cv2.imread('find_waldo.jpg')
+img_rgb = cv2.imread('waldo_finder/find_waldo.png')
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('waldo.png', 0)
+template = cv2.imread('waldo_finder/waldo.png', 0)
 
 w, h = template.shape[::-1] # What does this do?
