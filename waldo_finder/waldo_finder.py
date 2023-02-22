4809207eaf7b25d30855c18e7db22fa647e6549f
Logan Graham <LDGBK6@mail.missouri.edu>
2023-02-22 17:13:10 -0600

    Update waldo_finder.py

--- waldo_finder/waldo_finder.py
+++ waldo_finder/waldo_finder.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy
 
-img_rgb = cv2.imread('waldo_finder/find_waldo.png')
+img_rgb = cv2.imread('waldo_finder/find_waldo.png', cv2.IMREAD_UNCHANGED)
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('waldo_finder/waldo.png', 0)
+template = cv2.imread('waldo_finder/waldo.png', cv2.IMREAD_GRAYSCALE)
+h, w = template.shape
 
-w, h = template.shape[::-1] # What does this do?
+res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.6
+
+loc = numpy.where(res >= threshold)
+print(loc)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
+cv2.imwrite('found_waldo.png', img_rgb)
